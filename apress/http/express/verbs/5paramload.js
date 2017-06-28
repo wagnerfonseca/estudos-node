00ecbb4953284fea0954337aad64846b418f5fe8
Wagner Fonseca <wagner.fonseca@gmail.com>
2017-06-27 22:42:11 -0300

    paramload - como executar um middleware quando informado um parametro

--- apress/http/express/verbs/5paramload.js
+++ apress/http/express/verbs/5paramload.js
@@ -2,16 +2,19 @@ var express = require('express');
 
 var app = express();
 
+// uma vez que é informado um parametro este vai ser o middleware executado primeiro
 app.param('userId', function(req, res, next, userId) {
+    console.log('pesquisando o usario ...')
     res.write('pesquisando por usuario: ' + userId + '\n');
 
     // simula uma pesquisa por usuario e
     // carrega dentro de um objeto de requisição para um middleware superior
     req.user = {userId: userId };
-    next();
+    next(); // para devolver no middleware em que foi chamado
 });
 
 app.get('/user/:userId', function(req, res) {
+    console.log('chamando o usuario')
     res.end('Usuario é: ' + JSON.stringify(req.user));
 });
 
